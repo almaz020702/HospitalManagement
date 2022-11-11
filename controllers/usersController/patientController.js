140c9b20fcc170cf4e8abf2539d16edad7b04c59
kabdik <87753859226.ansar@gmail.com>
2022-11-11 18:18:21 +0600

    Routers redone and converstation controller added

---
+++ controllers/usersController/patientController.js
@@ -0,0 +1,15 @@
+const { Patient } = require("../../models/models")
+
+class PatientController{
+    async getOne(req,res,next){
+        try {
+            const {id}=req.user
+            const patient=await Patient.findOne({where:{id},attributes:{exclude:["password"]}})
+            return res.json(patient) 
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports=new PatientController()
