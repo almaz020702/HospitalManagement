fffafeabb880b8b683a4817b9d4a47d47ef38bf0
kabdik <87753859226.ansar@gmail.com>
2022-11-30 23:12:51 +0600

    appoinments for personal page patients

--- routes/usersRoutes/doctorRouter.js
+++ routes/usersRoutes/doctorRouter.js
@@ -1,8 +1,9 @@
 const Router=require("express")
+const appointmentController = require("../../controllers/appointmentController")
 const treatmentController = require("../../controllers/treatmentController")
 const doctorController = require("../../controllers/usersController/doctorController")
 const router=Router()
 router.get('/',doctorController.getOne)
 router.post('/treatment/:id',treatmentController.create)
-
+router.get('/appointment',appointmentController.getAll)
 module.exports=router
